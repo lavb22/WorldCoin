0d90b0f8429e2cc559ac710dfc9de91f3e443863
lavb22 <lavb22@gmail.com>
2018-01-08 11:28:11 -0400

    Uploaded Files

---
+++ src/keystore.cpp
@@ -0,0 +1,274 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin Developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "keystore.h"
+#include "script.h"
+
+bool CBasicKeyStore::GetPubKey(const CKeyID &address, CPubKey &vchPubKeyOut) const
+{
+    CKey key;
+    if (!GetKey(address, key)){
+    	LOCK(cs_KeyStore);
+    	WatchKeyMap::const_iterator it = mapWatchKeys.find(address);
+    	if (it != mapWatchKeys.end()) {
+    		vchPubKeyOut = it->second;
+    		return true; }
+    	return false;}
+    vchPubKeyOut = key.GetPubKey();
+    return true;
+}
+
+bool CKeyStore::AddKey(const CKey &key) {
+    return AddKeyPubKey(key, key.GetPubKey());
+}
+
+bool CBasicKeyStore::AddKeyPubKey(const CKey& key, const CPubKey &pubkey)
+{
+    LOCK(cs_KeyStore);
+    mapKeys[pubkey.GetID()] = key;
+    return true;
+}
+
+bool CBasicKeyStore::AddCScript(const CScript& redeemScript)
+{
+    LOCK(cs_KeyStore);
+    mapScripts[redeemScript.GetID()] = redeemScript;
+    return true;
+}
+
+bool CBasicKeyStore::HaveCScript(const CScriptID& hash) const
+{
+    LOCK(cs_KeyStore);
+    return mapScripts.count(hash) > 0;
+}
+
+bool CBasicKeyStore::GetCScript(const CScriptID &hash, CScript& redeemScriptOut) const
+{
+    LOCK(cs_KeyStore);
+    ScriptMap::const_iterator mi = mapScripts.find(hash);
+    if (mi != mapScripts.end())
+    {
+        redeemScriptOut = (*mi).second;
+        return true;
+    }
+    return false;
+}
+
+bool CCryptoKeyStore::SetCrypted()
+{
+    LOCK(cs_KeyStore);
+    if (fUseCrypto)
+        return true;
+    if (!mapKeys.empty())
+        return false;
+    fUseCrypto = true;
+    return true;
+}
+
+bool CCryptoKeyStore::Lock()
+{
+    if (!SetCrypted())
+        return false;
+
+    {
+        LOCK(cs_KeyStore);
+        vMasterKey.clear();
+    }
+
+    NotifyStatusChanged(this);
+    return true;
+}
+
+bool CCryptoKeyStore::Unlock(const CKeyingMaterial& vMasterKeyIn)
+{
+    {
+        LOCK(cs_KeyStore);
+        if (!SetCrypted())
+            return false;
+
+        CryptedKeyMap::const_iterator mi = mapCryptedKeys.begin();
+        for (; mi != mapCryptedKeys.end(); ++mi)
+        {
+            const CPubKey &vchPubKey = (*mi).second.first;
+            const std::vector<unsigned char> &vchCryptedSecret = (*mi).second.second;
+            CKeyingMaterial vchSecret;
+            if(!DecryptSecret(vMasterKeyIn, vchCryptedSecret, vchPubKey.GetHash(), vchSecret))
+                return false;
+            if (vchSecret.size() != 32)
+                return false;
+            CKey key;
+            key.Set(vchSecret.begin(), vchSecret.end(), vchPubKey.IsCompressed());
+            if (key.GetPubKey() == vchPubKey)
+                break;
+            return false;
+        }
+        vMasterKey = vMasterKeyIn;
+    }
+    NotifyStatusChanged(this);
+    return true;
+}
+
+bool CCryptoKeyStore::AddKeyPubKey(const CKey& key, const CPubKey &pubkey)
+{
+    {
+        LOCK(cs_KeyStore);
+        if (!IsCrypted())
+            return CBasicKeyStore::AddKeyPubKey(key, pubkey);
+
+        if (IsLocked())
+            return false;
+
+        std::vector<unsigned char> vchCryptedSecret;
+        CKeyingMaterial vchSecret(key.begin(), key.end());
+        if (!EncryptSecret(vMasterKey, vchSecret, pubkey.GetHash(), vchCryptedSecret))
+            return false;
+
+        if (!AddCryptedKey(pubkey, vchCryptedSecret))
+            return false;
+    }
+    return true;
+}
+
+
+bool CCryptoKeyStore::AddCryptedKey(const CPubKey &vchPubKey, const std::vector<unsigned char> &vchCryptedSecret)
+{
+    {
+        LOCK(cs_KeyStore);
+        if (!SetCrypted())
+            return false;
+
+        mapCryptedKeys[vchPubKey.GetID()] = make_pair(vchPubKey, vchCryptedSecret);
+    }
+    return true;
+}
+
+bool CCryptoKeyStore::GetKey(const CKeyID &address, CKey& keyOut) const
+{
+    {
+        LOCK(cs_KeyStore);
+        if (!IsCrypted())
+            return CBasicKeyStore::GetKey(address, keyOut);
+
+        CryptedKeyMap::const_iterator mi = mapCryptedKeys.find(address);
+        if (mi != mapCryptedKeys.end())
+        {
+            const CPubKey &vchPubKey = (*mi).second.first;
+            const std::vector<unsigned char> &vchCryptedSecret = (*mi).second.second;
+            CKeyingMaterial vchSecret;
+            if (!DecryptSecret(vMasterKey, vchCryptedSecret, vchPubKey.GetHash(), vchSecret))
+                return false;
+            if (vchSecret.size() != 32)
+                return false;
+            keyOut.Set(vchSecret.begin(), vchSecret.end(), vchPubKey.IsCompressed());
+            return true;
+        }
+    }
+    return false;
+}
+
+bool CCryptoKeyStore::GetPubKey(const CKeyID &address, CPubKey& vchPubKeyOut) const
+{
+    {
+        LOCK(cs_KeyStore);
+        if (!IsCrypted())
+            return CBasicKeyStore::GetPubKey(address, vchPubKeyOut);
+
+        CryptedKeyMap::const_iterator mi = mapCryptedKeys.find(address);
+        if (mi != mapCryptedKeys.end())
+        {
+            vchPubKeyOut = (*mi).second.first;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool CCryptoKeyStore::EncryptKeys(CKeyingMaterial& vMasterKeyIn)
+{
+    {
+        LOCK(cs_KeyStore);
+        if (!mapCryptedKeys.empty() || IsCrypted())
+            return false;
+
+        fUseCrypto = true;
+        BOOST_FOREACH(KeyMap::value_type& mKey, mapKeys)
+        {
+            const CKey &key = mKey.second;
+            CPubKey vchPubKey = key.GetPubKey();
+            CKeyingMaterial vchSecret(key.begin(), key.end());
+            std::vector<unsigned char> vchCryptedSecret;
+            if (!EncryptSecret(vMasterKeyIn, vchSecret, vchPubKey.GetHash(), vchCryptedSecret))
+                return false;
+            if (!AddCryptedKey(vchPubKey, vchCryptedSecret))
+                return false;
+        }
+        mapKeys.clear();
+    }
+    return true;
+}
+//Agregado para importaddress
+static bool ExtractPubKey(const CScript &dest, CPubKey& pubKeyOut)
+{
+std::vector<valtype> vSolutions;
+txnouttype whichType;
+if (!Solver(dest, whichType, vSolutions))
+return false;
+switch (whichType)
+{
+case TX_NONSTANDARD:
+return false;
+case TX_PUBKEY:
+pubKeyOut = CPubKey(vSolutions[0]);
+return true;
+case TX_PUBKEYHASH:
+return false;
+case TX_SCRIPTHASH:
+return false;
+case TX_MULTISIG:
+return false;
+}
+return false;
+//TODO: Use Solver to extract this?
+/* CScript::const_iterator pc = dest.begin();
+opcodetype opcode;
+std::vector<unsigned char> vch;
+if (!dest.GetOp(pc, opcode, vch) || vch.size() < 33 || vch.size() > 65)
+return false;
+pubKeyOut = CPubKey(vch);
+if (!pubKeyOut.IsFullyValid())
+return false;
+if (!dest.GetOp(pc, opcode, vch) || opcode != OP_CHECKSIG || dest.GetOp(pc, opcode, vch))
+return false;
+return true;*/
+}
+bool CBasicKeyStore::AddWatchOnly(const CScript &dest, const CKeyID &keyAdd)
+{
+LOCK(cs_KeyStore);
+CPubKey pubKey;
+setWatchOnly.insert(keyAdd);
+setScriptWatchOnly.insert(dest.GetID());
+if (ExtractPubKey(dest, pubKey))
+mapWatchKeys[pubKey.GetID()] = pubKey;
+return true;
+}
+bool CBasicKeyStore::RemoveWatchOnly(const CScript &dest)
+{
+LOCK(cs_KeyStore);
+setScriptWatchOnly.erase(dest.GetID());
+CPubKey pubKey;
+if (ExtractPubKey(dest, pubKey))
+mapWatchKeys.erase(pubKey.GetID());
+return true;
+}
+bool CBasicKeyStore::HaveWatchOnly(const CScript &dest) const
+{
+LOCK(cs_KeyStore);
+return setScriptWatchOnly.count(dest.GetID()) > 0;
+}
+bool CBasicKeyStore::HaveWatchOnly() const
+{
+LOCK(cs_KeyStore);
+return (!setWatchOnly.empty());
+}
---
+++ src/rpcdump.cpp
@@ -0,0 +1,151 @@
+// Copyright (c) 2009-2012 worldcoin Developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "init.h" // for pwalletMain
+#include "worldcoinrpc.h"
+#include "ui_interface.h"
+#include "base58.h"
+
+#include <boost/lexical_cast.hpp>
+
+#define printf OutputDebugStringF
+
+using namespace json_spirit;
+using namespace std;
+
+class CTxDump
+{
+public:
+    CBlockIndex *pindex;
+    int64 nValue;
+    bool fSpent;
+    CWalletTx* ptx;
+    int nOut;
+    CTxDump(CWalletTx* ptx = NULL, int nOut = -1)
+    {
+        pindex = NULL;
+        nValue = 0;
+        fSpent = false;
+        this->ptx = ptx;
+        this->nOut = nOut;
+    }
+};
+
+//#####Inicio de codigo agregado para importaddress
+void ImportScript(const CScript& script, const std::string& strLabel, const CKeyID& AddID)
+{
+if (IsMine(*pwalletMain,script)) {
+throw JSONRPCError(RPC_WALLET_ERROR, "The wallet already contains this address or script");
+}
+int64_t nCreateTime = 1;
+pwalletMain->MarkDirty();
+if (!pwalletMain->AddWatchOnly(script, 1 /* nCreateTime */, AddID)) {
+throw JSONRPCError(RPC_WALLET_ERROR, "Error adding address to wallet");
+}
+pwalletMain->SetAddressBookName(AddID, strLabel);
+}
+Value importaddress(const Array& params, bool fHelp)
+{
+if (fHelp || params.size() < 1 || params.size() > 4)
+throw std::runtime_error(
+"importaddress \"address\" ( \"label\" rescan )\n"
+"\nAdds a script (in hex) or address that can be watched as if it were in your wallet but cannot be used to spend.\n"
+"\nArguments:\n"
+"1. \"script\" (string, required) The hex-encoded script (or address)\n"
+"2. \"label\" (string, optional, default=\"\") An optional label\n"
+"3. rescan (boolean, optional, default=true) Rescan the wallet for transactions\n"
+"\nNote: This call can take minutes to complete if rescan is true.\n"
+"If you have the full public key, you should call importpubkey instead of this.\n"
+"\nNote: If you import a non-standard raw script in hex form, outputs sending to it will be treated\n"
+"as change, and not show up in many RPCs."
+);
+std::string strLabel = "";
+if (params.size() > 1)
+strLabel = params[1].get_str();
+// Whether to perform rescan after import
+bool fRescan = true;
+if (params.size() > 2)
+fRescan = params[2].get_bool();
+LOCK2(cs_main, pwalletMain->cs_wallet);
+CWorldcoinAddress coinAdd;
+coinAdd.SetString(params[0].get_str());
+CKeyID dest;
+if (coinAdd.IsValid() && coinAdd.GetKeyID(dest)) {
+CScript scriptAdd;
+scriptAdd.SetDestination(dest);
+ImportScript(scriptAdd, strLabel, dest);
+} else {
+throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Leocoin address");
+}
+if (fRescan)
+{
+pwalletMain->ScanForWalletTransactions(pindexGenesisBlock, true);
+pwalletMain->ReacceptWalletTransactions();
+}
+return Value::null;
+}
+//####Fin de codigo agregado para importaddress
+
+Value importprivkey(const Array& params, bool fHelp)
+{
+    if (fHelp || params.size() < 1 || params.size() > 3)
+        throw runtime_error(
+            "importprivkey <worldcoinprivkey> [label] [rescan=true]\n"
+            "Adds a private key (as returned by dumpprivkey) to your wallet.");
+
+    string strSecret = params[0].get_str();
+    string strLabel = "";
+    if (params.size() > 1)
+        strLabel = params[1].get_str();
+
+    // Whether to perform rescan after import
+    bool fRescan = true;
+    if (params.size() > 2)
+        fRescan = params[2].get_bool();
+
+    CBitcoinSecret vchSecret;
+    bool fGood = vchSecret.SetString(strSecret);
+
+    if (!fGood) throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid private key");
+
+    CKey key = vchSecret.GetKey();
+    CPubKey pubkey = key.GetPubKey();
+    CKeyID vchAddress = pubkey.GetID();
+    {
+        LOCK2(cs_main, pwalletMain->cs_wallet);
+
+        pwalletMain->MarkDirty();
+        pwalletMain->SetAddressBookName(vchAddress, strLabel);
+
+        if (!pwalletMain->AddKeyPubKey(key, pubkey))
+            throw JSONRPCError(RPC_WALLET_ERROR, "Error adding key to wallet");
+
+        if (fRescan) {
+            pwalletMain->ScanForWalletTransactions(pindexGenesisBlock, true);
+            pwalletMain->ReacceptWalletTransactions();
+        }
+    }
+
+    return Value::null;
+}
+
+Value dumpprivkey(const Array& params, bool fHelp)
+{
+    if (fHelp || params.size() != 1)
+        throw runtime_error(
+            "dumpprivkey <worldcoinaddress>\n"
+            "Reveals the private key corresponding to <worldcoinaddress>.");
+
+    string strAddress = params[0].get_str();
+    CWorldcoinAddress address;
+    if (!address.SetString(strAddress))
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Worldcoin address");
+    CKeyID keyID;
+    if (!address.GetKeyID(keyID))
+        throw JSONRPCError(RPC_TYPE_ERROR, "Address does not refer to a key");
+    CKey vchSecret;
+    if (!pwalletMain->GetKey(keyID, vchSecret))
+        throw JSONRPCError(RPC_WALLET_ERROR, "Private key for address " + strAddress + " is not known");
+    return CBitcoinSecret(vchSecret).ToString();
+}
